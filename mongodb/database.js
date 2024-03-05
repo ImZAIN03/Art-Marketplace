d22e33d06e6c83c8690654720dbdd1c54d537ed3
Muhammed Zain <muhammedzain2256@gmail.com>
2024-03-05 19:44:05 +0500

    User and Database

---
+++ mongodb/database.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let isConnected = false
+
+export const connectDB = async () => {
+    mongoose.set('strictQuery', true)
+
+    if (isConnected) {
+        console.log("MongoDB is connected successfully!")
+        return 
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            dbName: "artify",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        isConnected = true,
+
+        console.log("MongoDB Connected")
+    } catch (err) {
+        console.log(err);
+    }
+}
